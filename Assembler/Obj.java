e7212827105650e087b267daf7e7fc19d0a3ceab
Vivek Abhale <vivekabhale787@gmail.com>
2025-11-11 18:15:32 +0000

    Added new SPOS files

--- Assembler/Obj.java
+++
@@ -1,9 +0,0 @@
-public class Obj {
-    String name;
-    int addr;
-
-    Obj(String nm, int address) {
-        this.name = nm;
-        this.addr = address;
-    }
-}
--- Assembler/Pooltable.java
+++
@@ -1,8 +0,0 @@
-public class Pooltable {
-    int first, total_literals;
-
-    public Pooltable(int f, int l) { 
-        this.first = f;
-        this.total_literals = l;
-    }
-}
